85320d970fb29c89b00e1771827894864a2e96d2
LalbaAnthony <anthonylalaba24@gmail.com>
2026-03-11 00:14:32 +0100

    fix: misses on back

--- backend/src/middlewares/log.middleware.ts
+++ backend/src/middlewares/log.middleware.ts
@@ -1,14 +1,14 @@
 // src/middleware/log.middleware.ts
 import { Request, Response, NextFunction } from 'express'
-import { logService } from '../services/log.service'
 import { LogException } from '../exceptions/log.exception'
+import { Log } from '../utils/log.utils'
 
 export const bypasses = ['/', '/health', '/swagger']
 
 export const logMiddleware = async (req: Request, res: Response, next: NextFunction) => {
     try {
         if (bypasses.includes(req.originalUrl)) return next()
-        await logService.make(`[${req.method}] ${req.originalUrl}`, 'info', req)
+        await Log.info(`[${req.method}] ${req.originalUrl}`, req)
     } catch (error) {
         throw new LogException((error as Error).message ?? 'Logging error')
     }
