d0432bac00b17f7327c8013e861a5d18fa8aab1d
LalbaAnthony <anthonylalaba24@gmail.com>
2026-03-13 21:46:37 +0100

    feat: checkpoint OMG TIS WORKING MOM LOOK

---
+++ shared/src/index.ts
@@ -0,0 +1,2 @@
+export type { PostAttributes, PostAttributesCreation, PostAttributesUpdate } from './types/post.types'
+export { PostStatus } from './types/post.types'  // enum/const, pas "export type"
