31b72966b409133f93f9006024727b9921362cfd
LalbaAnthony <anthony.lalba@limayrac.fr>
2026-03-15 09:49:07 +0100

    feat: WIP comp

--- frontend/components/atoms/Wip.vue
+++ frontend/components/atoms/Wip.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex items-center justify-center w-full h-full bg-yellow-400 p-6 min-h-[200px]">
-        <span class="text-4xl font-black tracking-widest text-yellow-900 uppercase">
+        <span class="text-4xl font-black tracking-widest text-yellow-900 uppercase line-clamp-1">
             En cours de développement
         </span>
     </div>
