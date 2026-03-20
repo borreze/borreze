408a6b56301b8c5c75886595b6893f62bba0b01e
LalbaAnthony <anthonylalaba24@gmail.com>
2026-03-20 18:09:46 +0100

    feat: media

--- backend/src/middlewares/upload.middleware.ts
+++ backend/src/middlewares/upload.middleware.ts
@@ -2,14 +2,14 @@ import multer from 'multer'
 import { isTypeAllowed, MEDIA_UPLOAD_LIMIT } from '@brz/shared'
 import { MEDIA_UPLOAD_DIR } from '@brz/shared'
 import { MediaException } from '../exceptions/media.exception'
-import { makeFilenameUnique } from '../utils/media.utils'
+import { cleanFilename, makeFilenameUnique } from '../utils/media.utils'
 
 const storage = multer.diskStorage({
     destination: (_req, _file, cb) => {
         cb(null, MEDIA_UPLOAD_DIR)
     },
     filename: (_req, file, cb) => {
-        const newFilename = makeFilenameUnique(file.originalname, MEDIA_UPLOAD_DIR)
+        const newFilename = makeFilenameUnique(cleanFilename(file.originalname), MEDIA_UPLOAD_DIR)
         cb(null, newFilename)
     }
 })
--- backend/src/utils/media.utils.ts
+++ backend/src/utils/media.utils.ts
@@ -2,6 +2,22 @@ import fs from 'fs'
 import path from 'path'
 import { MediaException } from '../exceptions/media.exception'
 
+export const cleanFilename = (text: string | null | undefined): string => { // cnat use slugify since it removes too much chars, e.g. "photo de l'été.jpg" -> "photo-de-l-t-jpg"
+    if (!text) return ''
+
+    return text
+        .toString()
+        .normalize('NFD')                     // separate accent from letter
+        .replace(/[\u0300-\u036f]/g, '')      // remove all accents
+        .toLowerCase()
+        .trim()
+        .replace(/['’]/g, '-')                // replace apostrophes
+        .replace(/\s+/g, '-')                 // spaces -> -
+        .replace(/--+/g, '-')                 // multiple - -> single -
+        .replace(/^-+/, '')                   // trim - start
+        .replace(/-+$/, '')                   // trim - end
+}
+
 export function makeFilenameUnique(filename: string, dir: string): string {
     const MAX_UNIQUE_ATTEMPTS = 100
 
