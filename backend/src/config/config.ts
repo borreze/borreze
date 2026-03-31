098282b0d492cfef15a89655e6c1445f42661bc3
Anthony LALBA <a.lalba@agoravita.com>
2026-03-31 17:16:32 +0200

    fix: test pipe

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -6,11 +6,11 @@ dotenv.config()
 function required(key: string): string {
     const val = process.env[key];
 
-    if ((!val || val.trim() === '') && (process.env.NODE_ENV !== 'test')) {
+    if ((!val || val?.trim() === '') && (process.env.NODE_ENV !== 'test')) {
         throw new Error(`Missing required env variable: ${key}`);
     }
 
-    return val.trim();
+    return val?.trim() || '';
 }
 
 function optional(key: string, fallback: string): string {
